0c0142e664e37ce83e144da9f526df6728948324
Ronil Patil <63307564+ronylpatil@users.noreply.github.com>
2021-12-02 16:48:21 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -36,7 +36,7 @@ def main() :
 def predict_class(image) :
     with st.spinner('Loading, please be patient...') :
         # model = joblib.load(r'E:\DHL Project\CNN Projects\Deep Hybrid Learning Projects\X-ray\xray.pkl')
-        model = joblib.load(r'xray.pkl')
+        model = joblib.load('xray.pkl')
         vggmodel = VGG16(weights = 'imagenet', include_top = False, input_shape = (256, 256, 3))
         for i in vggmodel.layers :
             i.trainable = False
